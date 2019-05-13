74bc205e50b78a7e359832461826a84c38ad0c1b
Yue Zhang <yjzhang@cs.washington.edu>
2019-05-12 22:05:22 -0700

    preprocessing

--- preprocess_mca_microwell_seq.py
+++ preprocess_mca_microwell_seq.py
@@ -7,13 +7,36 @@ import pandas as pd
 import scipy.io
 from scipy import sparse
 
+class GeneNameTransform(object):
+    # TODO
+
+    def __init__(self, old_gene_names, new_gene_names):
+        """
+        Args:
+            old_gene_names (array of str)
+            new_gene_names (array of str)
+        """
+        self.old_to_unified = []
+        self.new_to_unified = []
+
+    def transform_old(self, new_data):
+        """
+        Transforms a dataset of the "old" gene names to the unified form.
+        """
+
+    def transform_new(self, new_data):
+        """
+        """
+
 annotations = pd.read_csv('mca_microwell_seq/MCA_CellAssignments.csv')
 
 current_batch = None
 current_tissue = None
+current_table = None
 current_matrix = None
 current_barcodes = None
 current_genes = None
+prev_cell_type = None
 cell_type_values = {}
 cell_type_genes = {}
 # TODO: try to align gene names across dataset?
@@ -21,27 +44,32 @@ for index, row in annotations.iterrows():
     try:
         batch = row.Batch
         tissue = row.Tissue
+        cell_barcode = row['Cell.Barcode']
+        cell_type = row['Annotation']
         if batch != current_batch:
             dirname = ''.join(batch.split('_'))
             current_tissue = tissue
             current_batch = batch
             print(batch)
             try:
-                current_matrix = pd.read_table(os.path.join('mca_microwell_seq', 'rmbatch_dge', '{0}_rm.batch_dge.txt.gz'.format(dirname)), sep=' ')
+                current_table = pd.read_table(os.path.join('mca_microwell_seq', 'rmbatch_dge', '{0}_rm.batch_dge.txt.gz'.format(dirname)), sep=' ')
             except:
                 dirname = batch.split('_')[0]
-                current_matrix = pd.read_table(os.path.join('mca_microwell_seq', 'rmbatch_dge', '{0}_rm.batch_dge.txt.gz'.format(dirname)), sep=' ')
-            current_barcodes = np.array([x.split('.')[1] for x in current_matrix.columns])
-            current_genes = current_matrix.index.values.astype(str)
-            current_matrix = current_matrix.values
-        cell_barcode = row['Cell.Barcode']
+                current_table = pd.read_table(os.path.join('mca_microwell_seq', 'rmbatch_dge', '{0}_rm.batch_dge.txt.gz'.format(dirname)), sep=' ')
+            if prev_cell_type == cell_type:
+                # TODO: figure out how the genes should work?
+                old_genes = current_genes
+                pass
+            current_barcodes = np.array([x.split('.')[1] for x in current_table.columns])
+            current_genes = current_table.index.values.astype(str)
+            current_matrix = current_table.values
         index = np.where(current_barcodes == cell_barcode)[0][0]
-        cell_type = row['Annotation']
         if cell_type not in cell_type_values:
             cell_type = cell_type.replace('/', '-')
             cell_type_values[cell_type] = []
             cell_type_genes[cell_type] = current_genes
         cell_type_values[cell_type].append(current_matrix[:, index])
+        prev_cell_type = cell_type
     except:
         continue
 
